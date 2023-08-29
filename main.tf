666341937632214be55837d78fc699b7ed955805
Alexis VIALARET <alexis.vialaret@artefact.com>
2023-08-29 14:01:48 +0200

    doc: fleshed out "usage" and "architecture" sections

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 /*
-* # terraform-google-airbyte-flows
+* # terraform-google-airbyte-infra
 *
 * A Terraform module to deploy an Airbyte server on a GCP compute engine instance.
 *
@@ -43,11 +43,27 @@
 *
 * ## Usage
 *
-* To the `examples` directory to view deployment code samples.
+* [Go to the `examples` directory to view deployment code samples.](https://github.com/artefactory/terraform-google-airbyte-infra/tree/main/examples)
+*
+* Once deployment is successfully finished, you will need to SSH tunnel to your Airbyte instance:
+*
+* ```shell
+* $ gcloud compute ssh airbyte -- -L 8000:localhost:8000 -N -f
+* ```
+*
+* [You will then be able to access it via your browser](http://localhost:8000).
+*
+* You may also Terraform the ELT flows themselves using the [Artefactory terraform-google-airbyte-flows module](https://registry.terraform.io/modules/artefactory/airbyte-flows/google/latest), or the [Airbyte community provider](https://registry.terraform.io/providers/josephjohncox/airbyte/latest).
 *
 * ## Architecture
 *
+* This module will provision the following resources.
+*
 * ![](docs/archi.png)
+*
+* As a security precaution, the Airbyte VM is not reachable from the internet. The only way to access it is through gcloud-authenticated SSH. Egress traffic is of course possible to access remote data sources to pull data from.
+*
+* The Airbyte service account has fairly high level of privilege on GCS and BQ (`roles/storage.objectAdmin`, and `roles/bigquery.dataEditor`), allowing it to read/write on any table or bucket. There permissions are required to allow Airbyte to create temporary datasets and tables. If that is an issue for you, you can isolate this deployment in a dedicated project.
 *
 * --------
 * ## Auto-generated module documentation
